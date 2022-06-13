6731ea450ab84a18ff5dcea4ac1a82ea34387b12
Karen Elpidio <Karen73@live.com.ar>
2022-06-13 12:53:12 -0300

    Plantillas

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func main() {
 	http.HandleFunc("/", Inicio)
-	log.Println("Servidor corriendo...")
+	http.HandleFunc("/crear", Crear)
 
+	log.Println("Servidor corriendo...")
+	http.ListenAndServe(":8080", nil)
 }
 
-func Inicio(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Holaaaaa")
+func Inicio (w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hola Karen")
+	plantillas.ExecuteTemplate(w, "inicio", nil)
+
+}
+func Crear (w http.ResponseWriter, r *http.Request) {
+		plantillas.ExecuteTemplate(w, "crear", nil)
 
 }
